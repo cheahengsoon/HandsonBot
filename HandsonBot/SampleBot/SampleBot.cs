bc4d5ee2a285b66a245ab5cc26934e4e5d6b525e
shinichi.nishihata <shinichi.nishihata@gmail.com>
2018-10-05 17:47:27 +0900

    feat:ウェルカムメッセージの実装

---
+++ HandsonBot/SampleBot/SampleBot.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace HandsonBot.SampleBot
+{
+    public class SampleBot : IBot
+    {
+        private const string WelcomeText = "SampleBot へようこそ！";
+
+        private readonly ILogger _logger;
+
+        public SampleBot(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SampleBot>();
+            _logger.LogInformation("Start SampleBot");
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // 通常のメッセージのやり取りはここで行います。
+                await SendMessageActivityAsync(turnContext, cancellationToken);
+            }
+            else if (turnContext.Activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                await SendWelcomeMessageAsync(turnContext, cancellationToken);
+            }
+            else
+            {
+                _logger.LogInformation($"passed:{turnContext.Activity.Type}");
+            }
+        }
+
+        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(WelcomeText, cancellationToken: cancellationToken);
+                }
+            }
+        }
+
+        private async Task SendMessageActivityAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"passed:{turnContext.Activity.Type}");
+        }
+
+    }
+}
